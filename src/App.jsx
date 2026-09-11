17e88bc81c3681975c2c778f81aafe9e6849d9f3
Felippe Tuma <felippetuma@gmail.com>
2026-09-11 09:51:26 -0300

    style: Estilizando componente

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,25 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom" 
+import Navbar from "./components/Navbar"
+import Home from "./pages/Home"
+import Sobre from "./pages/Sobre"
+import Contato from "./pages/Contato"
+import Produtos from "./pages/Produtos"
+
 function App() {
   return (
-    <>
-    </>
+    <Router>
+      <div>
+        <Navbar/>
+        <main>
+          <Routes>
+            <Route path="/" element={<Home/>}/>
+            <Route path="/sobre" element={<Sobre/>}/>
+            <Route path="/produtos" element={<Produtos/>}/>
+            <Route path="/contatos" element={<Contato/>}/>
+          </Routes>
+        </main>
+      </div>
+    </Router>
   )
 }
 
